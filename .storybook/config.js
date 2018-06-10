2b3414ca2e80aca35d4858ef607bf8e57bad9898
Ivan Santos <301291+pragmaticivan@users.noreply.github.com>
2018-06-09 22:58:44 -0700

    Fixes deprecated config

--- .storybook/config.js
+++ .storybook/config.js
@@ -22,12 +22,12 @@ setOptions({
    * Display left panel that shows a list of stories
    * @type {Boolean}
    */
-  showLeftPanel: true,
+  showStoriesPanel: true,
   /**
    * Display horizontal panel that displays addon configurations
    * @type {Boolean}
    */
-  showDownPanel: false,
+  showAddonPanel: true,
   /**
    * Display floating search box to search through stories
    * @type {Boolean}
@@ -37,7 +37,7 @@ setOptions({
    * Show horizontal addons panel as a vertical panel on the right
    * @type {Boolean}
    */
-  downPanelInRight: false,
+  addonPanelInRight: false,
   /**
    * Sorts stories
    * @type {Boolean}
